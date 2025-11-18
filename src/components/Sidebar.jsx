b1e1b1949a117401cac6749023d95dc4fbf666bb
Najath <najathniju007@gmail.com>
2025-11-18 11:29:49 +0530

    button size increase

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Sidebar.css"
+import { Link } from 'react-router-dom'
 
 export default function Sidebar() {
+
+  const [active,setActive] = useState("")
+
   return (
-    <div className='sidebar'>
-      <div>
-        <ul className='text-white list-unstyled justify-content-between align-items-center'>
-           <li>HOME</li>
-           <li>ABOUT</li>
-           <li>CONTENT</li>
+    <div className='sidebar p-5'>
+        <ul className='list-unstyled t-l'>
+
+           <li className='mb-3'>
+             <Link 
+               to="/Home" 
+               className={`text-decoration-none text-white ${active === "home" ? "fs-2" : "fs-4"}`}
+               onClick={() => setActive("home")}
+             >
+               Home
+             </Link>
+           </li>
+
+           <li className='mb-3'>
+             <Link 
+               to="/About" 
+               className="text-decoration-none text-white"
+               onClick={() => setActive("about")}
+             >
+               About
+             </Link>
+           </li>
+
+           <li>
+             <Link 
+               to="/Contact" 
+               className="text-decoration-none text-white"
+               onClick={() => setActive("contact")}
+             >
+               Contact
+             </Link>
+           </li>
+
         </ul>
-       
-      </div>
     </div>
   )
 }
