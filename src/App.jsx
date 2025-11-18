93ac35daf2ed9ea725f12f858f66eb593f42aae5
Najath <najathniju007@gmail.com>
2025-11-18 10:29:19 +0530

    added pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './components/Home'
-import AddExpencees from './components/AddExpences'
+import About from './components/About' 
+import Contact from './components/Contact'
+import Sidebar from './components/Sidebar'
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
+  return (
+    <div >
+      <Sidebar />
+      <div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Home' element={<Home />} />
+          <Route path='/About' element={<About />} />
+          <Route path='/Contact' element={<Contact />} />
+        </Routes>
+      </div>
+    </div>
 
- <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='/AddExpences' element={<AddExpencees />} />
- </Routes>
-
+  )
 
 }
 
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Sidebar.css"
+
+export default function Sidebar() {
+  return (
+    <div className='sidebar'>
+      <div>
+        <ul className='text-white list-unstyled justify-content-between align-items-center'>
+           <li>HOME</li>
+           <li>ABOUT</li>
+           <li>CONTENT</li>
+        </ul>
+       
+      </div>
+    </div>
+  )
+}
